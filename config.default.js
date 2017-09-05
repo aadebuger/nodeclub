881ff2d6380fe9b11494c084f3a59e34118e3c8a
hua zhuang <zh@zqf.com.cn>
2017-09-05 18:11:02 +0800

    mongo

--- config.default.js
+++ config.default.js
@@ -137,7 +137,7 @@ var config = {
 };
 
 if (process.env.NODE_ENV === 'test') {
-  config.db = 'mongodb://127.0.0.1/node_club_test';
+  config.db = 'mongodb://mongodb/node_club_test';
 }
 
 module.exports = config;
